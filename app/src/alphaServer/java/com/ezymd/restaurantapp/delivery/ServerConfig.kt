d820a3cf5cd62c468cd841dae39f050e247daace
m_tanwar <mohit.tanwar@kelltontech.com>
2021-06-26 10:41:55 +0530

    base url change

--- app/src/alphaServer/java/com/ezymd/restaurantapp/delivery/ServerConfig.kt
+++ app/src/alphaServer/java/com/ezymd/restaurantapp/delivery/ServerConfig.kt
@@ -8,7 +8,7 @@ interface ServerConfig {
 
         const val IS_TESTING = true
         const val facebook = "JsmOX78jPApjOOP7ozZxtZL/Yto="
-        const val BASE_URL = "http://34.121.92.176/ezymd/public/api/"
+        const val BASE_URL = "http://app.ezymd.com/api/"
 
         const val OTP_HASH = "FCWFd3gBNu0"
 
@@ -16,22 +16,22 @@ interface ServerConfig {
         const val GENERATE_OTP = "sendOtp"
         const val LIST_BANNER = "nearByRestaurantBanner"
         const val LOGOUT = "logout"
-        const val LIST_RESTURANTS="nearByRestaurant"
-        const val SEARCH_RESTURANTS="nearByRestaurant"
-        const val RESTURANT_DETAILS="restaurantItems/{id}"
+        const val LIST_RESTURANTS = "nearByRestaurant"
+        const val SEARCH_RESTURANTS = "nearByRestaurant"
+        const val RESTURANT_DETAILS = "restaurantItems/{id}"
         const val SOCIAL_LOGIN_USER = "socialLoginRegister"
         const val LOGIN_USER = "agentlogin"
-        const val LIST_TRENDING="trendingFoods"
+        const val LIST_TRENDING = "trendingFoods"
 
 
-        const val DIRECTION_API="https://maps.googleapis.com/maps/api/directions/json"
-        const val CREATE_ORDER="order"
-        const val UPDATED_COORDINATES="updateOrderLocation"
+        const val DIRECTION_API = "https://maps.googleapis.com/maps/api/directions/json"
+        const val CREATE_ORDER = "order"
+        const val UPDATED_COORDINATES = "updateOrderLocation"
         const val ASSIGN_ORDER_DELIVERY = "assignOrderToDeliveryAgent"
-        const val ACCEPT_ORDER="updateOrderStatus"
-        const val CHANGE_DUTY_STATUS="updateUserDuty"
+        const val ACCEPT_ORDER = "updateOrderStatus"
+        const val CHANGE_DUTY_STATUS = "updateUserDuty"
         const val UPDATE_PROFILE = "updateProfile"
         const val CHANGE_PASSWORD = "forgotPassword"
-        const val FAQ_URL="http://34.121.92.176/ezymd/public/faq"
+        const val FAQ_URL = "http://app.ezymd.com/faq"
     }
 }
